c285f266f2201e3eac8f881eaf5cbbde70398341
Yasin Uslu <nepjua@gmail.com>
2018-02-17 01:22:24 +0300

    Add eslintrc for linting

---
+++ api/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true,
+    "mocha": true
+  },
+  "extends": ["eslint:recommended"],
+  "plugins": [
+    "prettier"
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true
+    }
+  },
+  "rules": {
+    "comma-dangle": 0,
+    "arrow-parens": 0,
+    "prettier/prettier": ["error", { "trailingComma": "es5", "singleQuote": true }]
+  }
+};
