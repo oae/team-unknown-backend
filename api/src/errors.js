bfa5b673791e827c35e2efe45f06a03f5db1c473
Yasin Uslu <nepjua@gmail.com>
2018-02-17 07:04:10 +0300

    Add validateNumber with ValidationError

--- api/src/errors.js
+++ api/src/errors.js
@@ -2,3 +2,4 @@ const BaseError = class BaseError extends Error {};
 
 exports.UnknownError = class UnknownError extends BaseError {};
 exports.MakerNotFoundError = class MakerNotFoundError extends BaseError {};
+exports.ValidationError = class ValidationError extends BaseError {};
--- api/src/utils.js
+++ api/src/utils.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+
+const { ValidationError } = require('./errors');
+
 function latLngToPoint(lat, lng) {
   return {
     type: 'Point',
@@ -5,6 +9,27 @@ function latLngToPoint(lat, lng) {
   };
 }
 
+function validateNumber({ number, min, max, fieldName }) {
+  try {
+    if (!_.isFinite(number)) {
+      throw `must be a number`;
+    }
+
+    if (number < min) {
+      throw `cannot be less than ${min}`;
+    }
+
+    if (number > max) {
+      throw `cannot be more than ${max}`;
+    }
+  } catch (errMessage) {
+    const error = new ValidationError(`${fieldName} ${errMessage}`);
+    error.payload = { fieldName };
+    throw error;
+  }
+}
+
 module.exports = {
   latLngToPoint,
+  validateNumber,
 };
