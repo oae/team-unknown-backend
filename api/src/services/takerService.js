a07ae7be57d3031e9c63188ee34bcea980a2306e
Alperen Elhan <alperen@elhan.org>
2018-02-17 06:11:10 +0300

    Create a job when a new withdrawal requested

--- api/src/services/takerService.js
+++ api/src/services/takerService.js
@@ -4,6 +4,7 @@ const bluebird = require('bluebird');
 const { Withdrawal, Taker } = bluebird.promisifyAll(require('../model'));
 const { WithdrawalStatus, MAXIMUM_WITHDRAWAL_AMOUNT } = require('../constants');
 const { latLngToPoint } = require('../utils');
+const { findMaker } = require('../queue');
 
 async function createWithdrawal(body, params) {
   const { deviceId } = params;
@@ -20,8 +21,6 @@ async function createWithdrawal(body, params) {
 
   const taker = await Taker.findOneAsync({ deviceId });
 
-  debug('Created a new withdrawal for %s', deviceId);
-
   const withdrawal = new Withdrawal({
     status: WithdrawalStatus.PENDING,
     amount,
@@ -29,6 +28,18 @@ async function createWithdrawal(body, params) {
   });
 
   await withdrawal.saveAsync();
+
+  debug('Created a new withdrawal for %s', deviceId);
+
+  const job = await findMaker
+    .createJob({
+      withdrawalId: withdrawal.id,
+    })
+    .retries(5)
+    .backoff('exponential', 1000)
+    .save();
+
+  debug('Created a findMaker job with the id %d', job.id);
 
   return withdrawal;
 }
