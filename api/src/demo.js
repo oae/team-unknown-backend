e3b1d56806cc4b6c66700538c9059b10299e42fd
Alperen Elhan <alperen@elhan.org>
2018-02-18 05:16:36 +0300

    Make demo users offline by default

--- api/src/demo.js
+++ api/src/demo.js
@@ -46,7 +46,7 @@ async function createTaker(index) {
 async function createMaker(index) {
   const user = await createDemoUser('demo-maker', index);
   user.maker = {
-    online: true,
+    online: false,
     minAmount: MAKER_MIN_AMOUNT,
     maxAmount: MAKER_MAX_AMOUNT,
     range: MAKER_MAX_RANGE,
